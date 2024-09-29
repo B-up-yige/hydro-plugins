f31671afa64df7481f1815f3a12fe23a7de87519
B-up-yige <1534493926@qq.com>
2024-09-29 20:11:04 +0800

    修复了ioi赛制无法抽奖的bug

--- lottery draw.js
+++ lottery draw.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         hydro抽奖小插件
 // @namespace    http://tampermonkey.net/
-// @version      1.0.4
+// @version      1.1.0
 // @description  https://github.com/B-up-yige/hydro-plugins
 // @author       yige123
 // @homepage     https://github.com/B-up-yige/hydro-plugins
 // @downloadURL  https://github.com/B-up-yige/hydro-plugins/raw/refs/heads/main/lottery%20draw.js
 // @updateURL    https://github.com/B-up-yige/hydro-plugins/raw/refs/heads/main/lottery%20draw.js
 
-// @match        *://www.gxustoj.com/*/contest/*/scoreboard
-// @match        *://www.gxustoj.com/contest/*/scoreboard
+// @match        *://*gxustoj.com/*/contest/*/scoreboard
+// @match        *://*gxustoj.com/contest/*/scoreboard
 
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gxustoj.com
 // @grant        none
@@ -21,11 +21,13 @@
     // Your code here...
     console.log("catch!");
     var board = document.getElementsByTagName("tbody")[0];
-    var score = board.getElementsByClassName("col--solved");
+    var acm = board.getElementsByClassName("col--solved");
+    var ioi = board.getElementsByClassName("col--total_score");
     var rank = board.getElementsByClassName("col--rank");
     var user = board.getElementsByClassName("col--user");
     var res;
 
+    //转换文本为临时文件
     function downloadTextFile(text, fileName) {
         const blob = new Blob([text], { type: 'text/plain' });
         const url = URL.createObjectURL(blob);
@@ -41,6 +43,7 @@
         URL.revokeObjectURL(url);
     }
 
+    //储存结果
     function save(){
         var em = res.sort();
         var resName = [];
@@ -50,16 +53,29 @@
         downloadTextFile(resName.join("\n"), "抽奖结果");
     }
 
+    //开始抽奖
     function start(){
 
         var valid = [];
-        for(var i = 0; i < score.length; i++){
-            var solve = Number(score[i].getElementsByTagName("span")[0].innerText.split("\n")[0]);
-            var isRank = (rank[i].getElementsByTagName("span")[0].innerText != "*");
-            if(solve > 0 && isRank){
-                valid.push(i);
+
+        if(acm.length){
+            for(var i = 0; i < acm.length; i++){
+                var solve = Number(acm[i].getElementsByTagName("span")[0].innerText.split("\n")[0]);
+                var isRank = (rank[i].getElementsByTagName("span")[0].innerText != "*");
+                if(solve > 0 && isRank){
+                    valid.push(i);
+                }
+            }
+        }else{
+            for(var i = 0; i < ioi.length; i++){
+                var solve = Number(ioi[i].getElementsByTagName("span")[0].innerText);
+                var isRank = (rank[i].getElementsByTagName("span")[0].innerText != "*");
+                if(solve > 0 && isRank){
+                    valid.push(i);
+                }
             }
         }
+        
         console.log(valid);
 
         res = valid.sort(() => Math.random() - 0.5).slice(0, 4);
